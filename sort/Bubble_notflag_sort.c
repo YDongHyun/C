d9693d3ab8b5c121b6dfacabc393665ec5fb2d55
양동현 <80799025+YDongHyun@users.noreply.github.com>
2021-11-29 23:31:11 +0900

    Update Bubble_notflag_sort.c

--- sort/Bubble_notflag_sort.c
+++ sort/Bubble_notflag_sort.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
-void bubble_a_sort(data,n)
-int data[],n;
-{
-  int i,k,tmp,j;
-  n=n-1;
-  for(i=1;i<n;i++){
-    for(k=0;k<n-i;k++){
-      if(data[k]>data[k+1]){
-        tmp=data[k];
-        data[k]=data[k+1];
-        data[k+1]=tmp;
-      }
-    }
-  }
+main() {
+    int ken[] = {3,5,9,7,1};
+    int i, cnt;
+    cnt = sizeof(ken) / sizeof(int);
+    bubble_a_sort(ken, cnt);
+
+    for (i = 0; i < cnt; i++)
+        printf("%3d", ken[i]);
+  
 }
 
-int main(){
-  int ken[]={3,5,9,7,1};
-  int i,cnt;
-  cnt=sizeof(ken)/sizeof(int);
-  bubble_a_sort(ken,cnt);
-  for(i=0;i<cnt;i++){
-    printf("%3d",ken[i]);
+bubble_a_sort(data, n) 
+int data[], n;
+ {
+   int i, k, tmp, j;
+   n = n - 1;
+   for (i = 0; i < n; i++) {
+      for (k = 0; k < n - i; k++) {
+         if (data[k] > data[k + 1]) {
+            tmp = data[k];
+            data[k] = data[k + 1];
+            data[k + 1] = tmp;
+                }
+            }
+        }
   }
-  getchar();
-}
