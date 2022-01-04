d390d62ef2b6af0feac99dd4e0be37ed69df0514
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-01-04 22:36:15 +0900

    Update beak_3036.c

--- beak/beak_3036.c
+++ beak/beak_3036.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include<stdlib.h>
 
+int greatest_common_factor(int a,int b){
+	int r;
+	if(a>b){
+	}else{
+		r=a;
+		a=b;
+		b=r;
+	}
+		while(1){
+			int tmp=b;
+			b=a%b;
+			a=tmp;
+			if(b==0){
+				break;
+			}
+		
+		}
+		return a;
+}
+
+
+void abbreviation(int bunja, int bunmo){
+	int a,b,k;
+	k=greatest_common_factor(bunja,bunmo);
+	a=bunja/k;
+	b=bunmo/k;
+	printf("%d/%d",a,b);
+	}
+
+int main(){
+	int N;
+	scanf("%d",&N);
+	
+	int* arr = malloc(sizeof(int)*N);
+	
+	for (int i=0;i<N;i++){
+		scanf("%d",&arr[i]);
+	}
+	
+	for(int j=1;j<N;j++){
+		abbreviation(arr[0],arr[j]);
+		if(j!=N-1){
+			printf("\n");
+		}
+	}
+}
