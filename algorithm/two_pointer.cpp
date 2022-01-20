cba1347e668f43e5fb3b54db1fba4f88908874cd
양동현 <80799025+YDongHyun@users.noreply.github.com>
2022-01-20 22:40:34 +0900

    Create two_pointer.cpp

---
+++ algorithm/two_pointer.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int n, m, i ,p1=0,p2=0,p3=0;
+	scanf("%d", n);
+	vector<int> a(n);
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	sort(a.begin(), a.end()); //퀵정렬 내림차순 함수 
+	scanf("%d", &m);
+	vector<int> b(m), c(n+m);
+	for(i=0;i<n;i++){
+		scanf("%d",&b[i]);
+	}	
+	sort(b.begin(), b.end());
+	
+	while(p1<n&&p2<m){
+		if(a[p1]==b[p2]){
+			c[p3++]=a[p1++];
+			p2++;
+		}
+		else if (a[p1]<b[p2]) p1++;
+		else p2++;
+	}
+	for(i=0;i<n;i++){
+		printf("%d ",c[i]);
+	}
+	return 0;
+}
